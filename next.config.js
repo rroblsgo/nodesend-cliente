969bee3abb2af9621806756c926183c85026d733
Ricardo Robles <rroblesgo@gmail.com>
2022-02-17 20:09:34 +0100

    Modificado next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     backendURL: 'https://boiling-forest-62158.herokuapp.com',
-    frontendURL: 'https://nodesend-cliente-eight-amber.vercel.app',
+    frontendURL: 'https://nodesend-cliente-3e0falykd-rroblesgo.vercel.app',
   },
 };
